9d8ee7abe069430a088d2d10daf30ad0961f9060
Jani Saari <aa3213@student.jamk.fi>
2026-04-28 14:28:59 +0300

    Added basic lazy loading and example of children-based loading

--- frontend/src/app/core/layout/page-layout/page-layout.ts
+++ frontend/src/app/core/layout/page-layout/page-layout.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { NavBar } from "../nav-bar/nav-bar";
-import { RouterOutlet } from "../../../../../node_modules/@angular/router/types/_router_module-chunk";
+import { NavBar } from '../nav-bar/nav-bar';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-page-layout',
