88ecad4f9e610d7b2141a88384e70fbe2fc1f712
Jani Saari <aa3213@student.jamk.fi>
2026-04-28 14:43:25 +0300

    Added login-page login form

--- frontend/src/app/features/auth/login/login-page.ts
+++ frontend/src/app/features/auth/login/login-page.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginPage {}
+export class LoginPage {
+  private fb = inject(FormBuilder);
+
+  // Initialize the reactive form with email and password controls
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      // Pass these values to your Cognito AuthService later
+      console.log('Form Submitted', this.loginForm.value);
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
+}
