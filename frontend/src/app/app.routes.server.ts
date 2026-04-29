5a71e2e6026a838fcbb7e63e0cb9a7766bca12e0
Jani Saari <aa3213@student.jamk.fi>
2026-04-29 11:21:45 +0300

    Updated routing and added more children. Made hybrid routing, will need to update

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -1,8 +1,29 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  // 1. The Welcome page (path: '')
+  {
+    path: '',
+    renderMode: RenderMode.Prerender,
+  },
+  // 2. The Signin page (path: 'signin')
+  {
+    path: 'signin',
+    renderMode: RenderMode.Prerender,
+  },
+  // 3. Register page (assuming it's 'register' under the 'auth' lazy loaded route)
+  {
+    path: 'auth/register', // Make sure this matches whatever is in auth.routes.ts!
+    renderMode: RenderMode.Prerender,
+  },
+  // 4. The Dashboard (Pure SPA)
+  {
+    path: 'dashboard/**',
+    renderMode: RenderMode.Client,
+  },
+  // 5. Global Fallback
   {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Server,
+  },
 ];
---
+++ frontend/src/app/features/dashboard/boards/boards-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boards-page',
+  imports: [],
+  templateUrl: './boards-page.html',
+  styleUrl: './boards-page.css',
+})
+export class BoardsPage {}
