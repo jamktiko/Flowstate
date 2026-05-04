fe154581a08b8468c131fc990379b2f87b819981
annikajarvinen <ag3931@student.jamk.fi>
2026-05-04 13:43:41 +0300

    Loginpage ready and small changes to registerpage.

--- frontend/src/app/core/layout/auth-layout/auth-layout.ts
+++ frontend/src/app/core/layout/auth-layout/auth-layout.ts
@@ -1,17 +1,14 @@
-import { NgOptimizedImage } from '@angular/common';
-import { Component, ChangeDetectionStrategy, input } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-auth-layout',
   templateUrl: './auth-layout.html',
   styleUrl: './auth-layout.css',
-  imports: [NgOptimizedImage],
+  imports: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AuthLayout {
   // Define 'title' as a required signal input
-  title = input.required<string>();
-
   // OR define it with a default value:
   // title = input<string>('Welcome');
 }
