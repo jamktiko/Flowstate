11adaaee14280bc4ac67dc3ee695c561aa25c4dc
Jani S <aa3213@student.jamk.fi>
2026-05-05 10:44:19 +0300

    Fixed a cognito bug in environment.development.ts

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,1 +1,7 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  cognito: {
+    userPoolId: 'eu-north-1_SfWo3EkIs',
+    userPoolClientId: '4obh8krimbm973e83gte5sfgh1',
+  },
+};
