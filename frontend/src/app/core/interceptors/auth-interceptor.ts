99165b3720e69484cb13d2b8662e3065f13399d2
Jani Saari <aa3213@student.jamk.fi>
2026-05-06 10:12:07 +0300

    Changed canActivateFn to canMatchFn. Added base-api url to environments. Checks now whether user is sending token to our api.

--- frontend/src/app/core/interceptors/auth-interceptor.ts
+++ frontend/src/app/core/interceptors/auth-interceptor.ts
@@ -2,9 +2,14 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from '../auth/auth-service';
 import { from, switchMap } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
+
+  if (!req.url.startsWith(environment.apiBaseUrl)) {
+    return next(req);
+  }
 
   return from(authService.getTokens()).pipe(
     switchMap((tokens) => {
